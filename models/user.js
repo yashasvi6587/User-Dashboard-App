170dc4f881cbbb65434ef373abd755ceb8678d0f
Yashasvi Sharma <yashharit1991@gmail.com>
2025-06-01 23:12:47 +0530

    Update user.js

--- models/user.js
+++ models/user.js
@@ -1,6 +1,6 @@
 const mongoose = require("mongoose")
 
-mongoose.connect("mongodb://localhost:27017/lecture17")
+mongoose.connect(process.env.MONGO_URI)
 
 const UserSchema = mongoose.Schema({
     username:String,
